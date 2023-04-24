b403ee2a3bc7894c57a4ec525c6bc37e8dce9e31
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-04-24 16:03:29 -0400

    First Javascript code

---
+++ SecondCode.js
@@ -0,0 +1,23 @@
+// The following is a script
+// that does a few things.
+
+/*
+The following is a script
+that does a few things
+*/
+
+"use strict"
+
+var firstName;
+
+firstName = "Deshonda";
+
+var greeting;
+
+greeting = "Your name is " firstName; // This line will assign the result of an expression to the greeting variable.
+
+console.log(greeting);
+
+firstName = "Deshonda";
+
+console.log("Your name is" + firstName);
